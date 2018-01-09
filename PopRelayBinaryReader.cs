d10855a4a5d1c07640511474211f718ebb92cd46
Graham Reeves <graham@grahamreeves.com>
2018-01-09 13:33:11 +0000

    Added a "binary reader" which takes a whole binary file and treats it like a packet

---
+++ PopRelayBinaryReader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PopRelayBinaryReader : MonoBehaviour
+{
+	public PopRelayClient		_Client;
+	public PopRelayClient		Client { get { return (_Client!=null)?_Client:GetComponent<PopRelayClient>(); } }
+
+	public BasePacket			DataHeader;
+	public TextAsset			Data;
+
+
+	public void PopNextPacket()
+	{
+		//	make header
+		var Json = JsonUtility.ToJson(DataHeader);
+		var JsonBytes = System.Text.Encoding.UTF8.GetBytes(Json);
+
+		//	append binary data
+		var DataBytes = Data.bytes;
+
+		//	make total packet
+		var PacketBytes = new List<byte>();
+		PacketBytes.AddRange (JsonBytes);
+		PacketBytes.AddRange (DataBytes);
+	
+		var Packet = new PopMessageBinary (Data.bytes);
+		Packet.Data = PacketBytes.ToArray();
+
+		Client.OnMessageBinary.Invoke(Packet);
+	}
+
+	void Start()
+	{
+		PopNextPacket ();
+	}
+}
